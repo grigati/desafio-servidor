879317728690504c62ab7e1e3c95a961013b4d44
Esther <esther.bsb@gmail.com>
2019-05-19 09:26:18 -0300

    Adiciona payloads

---
+++ src/main/java/app/crud/payload/SolicitacaoLogin.java
@@ -0,0 +1,27 @@
+package app.crud.payload;
+
+import javax.validation.constraints.NotBlank;
+
+public class SolicitacaoLogin {
+    @NotBlank
+    private String cpf;
+
+    @NotBlank
+    private String senha;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
